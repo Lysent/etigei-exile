bb322739d342bdcf10d5680aa9447cf1daa45a53
Mitemi <miter360@gmail.com>
2025-02-02 21:27:43 -0500

    U1.83.6

--- scripts/content/tech/tech.js
+++ scripts/content/tech/tech.js
@@ -65,49 +65,126 @@ Events.on(ContentInitEvent, () => {
 
     // Neoulandia
     const neoulandia_timber = addTechNode({
-        parent: Vars.content.block("etigeox-colony-core"), //This crashes if it's a block (colony-core)
+        parent: Vars.content.block("etigeox-colony-core"),
+        objectives: [new Objectives.Produce(Vars.content.item("edt-tin"))],
         unlock: Vars.content.item("edt-timber"),
+        planet: "etigeox-Neoulandia"
+    });
+
+    const neoulandia_raw_ore = chainNode(neoulandia_timber, {
+        unlock: Vars.content.item("etigeox-raw-ore"),
+        objectives: [new Objectives.Produce(Vars.content.item("etigeox-raw-ore"))],
+        planet: "etigeox-Neoulandia"
+    });
+
+    const neoulandia_greater_ore = chainNode(neoulandia_raw_ore, {
+        unlock: Vars.content.item("etigeox-greater-ore"),
+        objectives: [new Objectives.Produce(Vars.content.item("etigeox-greater-ore"))],
         planet: "etigeox-Neoulandia"
     });
 
     const neoulandia_coal = addTechNode({
         parent: Vars.content.item("etigeox-raw-ore"),
-        objectives: [new Objectives.Produce(Items.coal)], //This crashess
+        objectives: [new Objectives.Produce(Items.coal)],
         unlock: Items.coal,
         planet: "etigeox-Neoulandia"
     });
+
     // const neoulandia_sand = addTechNode({
     //     parent: Vars.content.item("etigeox-timber"),
-    //     requirements: new Objectives.Produce(Items.sand),
+    //     objectives: [new Objectives.Produce(Items.sand)],
     //     unlock: Items.sand,
     //     planet: "etigeox-Neoulandia"
     // });
+
     const neoulandia_silicon = chainNode(neoulandia_coal, {
         objectives: [new Objectives.Produce(Items.silicon)],
         unlock: Items.silicon,
         planet: "etigeox-Neoulandia"
     });
 
-    // const neoulandia_water = addTechNode({
-    //     parent: Vars.content.item("edt-timber"),
-    //     objectives: [new Objectives.Produce(Liquids.water)],
-    //     unlock: Liquids.water,
-    //     planet: "etigeox-Neoulandia"
-    // });
-    // chainNode(neoulandia_water, {
-    //     unlock: Vars.content.item("etigeox-canned-water"),
-    //     planet: "etigeox-Neoulandia"
-    // });
-    // const neoulandia_oil = chainNode(neoulandia_water, {
-    //     unlock: Liquids.oil,
-    //     objectives: [new Objectives.Produce(Liquids.oil)],
-    //     planet: "etigeox-Neoulandia"
-    // });
-    // chainNode(neoulandia_oil, {
-    //     unlock: Liquids.slag,
-    //     requirements: new Objectives.Produce(Liquids.slag),
-    //     planet: "etigeox-Neoulandia"
-    // });
+
+    const neoulandia_tin = chainNode(neoulandia_raw_ore, {
+        unlock: Vars.content.item("etigeox-tin"),
+        objectives: [new Objectives.Produce(Vars.content.item("etigeox-tin"))],
+        planet: "etigeox-Neoulandia"
+    });
+    const neoulandia_alumin = chainNode(neoulandia_tin, {
+        unlock: Vars.content.item("etigeox-alumin"),
+        objectives: [new Objectives.Produce(Vars.content.item("etigeox-alumin"))],
+        planet: "etigeox-Neoulandia"
+    });
+    chainNode(neoulandia_alumin, {
+        unlock: Vars.content.item("etigeox-silver"),
+        objectives: [new Objectives.Produce(Vars.content.item("etigeox-silver"))],
+        planet: "etigeox-Neoulandia"
+    });
+
+    const neoulandia_m = chainNode(neoulandia_tin, {
+        unlock: Vars.content.item("etigeox-m-part"),
+        objectives: [new Objectives.Produce(Vars.content.item("etigeox-m-part"))],
+        planet: "etigeox-Neoulandia"
+    });
+    const neoulandia_t = chainNode(neoulandia_m, {
+        unlock: Vars.content.item("etigeox-t-part"),
+        objectives: [new Objectives.Produce(Vars.content.item("etigeox-t-part"))],
+        planet: "etigeox-Neoulandia"
+    });
+    chainNode(neoulandia_t, {
+        unlock: Vars.content.item("etigeox-smart-compound"),
+        objectives: [new Objectives.Produce(Vars.content.item("etigeox-smart-compound"))],
+        planet: "etigeox-Neoulandia"
+    });
+    chainNode(neoulandia_m, {
+        unlock: Vars.content.item("etigeox-d-part"),
+        objectives: [new Objectives.Produce(Vars.content.item("etigeox-d-part"))],
+        planet: "etigeox-Neoulandia"
+    });
+
+    chainNode(neoulandia_greater_ore, {
+        unlock: Vars.content.item("etigeox-nickel"),
+        objectives: [new Objectives.Produce(Vars.content.item("etigeox-nickel"))],
+        planet: "etigeox-Neoulandia"
+    });
+    chainNode(neoulandia_greater_ore, {
+        unlock: Vars.content.item("etigeox-platinum"),
+        objectives: [new Objectives.Produce(Vars.content.item("etigeox-platinum"))],
+        planet: "etigeox-Neoulandia"
+    });
+
+    const neoulandia_research1 = chainNode(neoulandia_timber, {
+        unlock: Vars.content.item("etigeox-research-materia1"),
+        objectives: [new Objectives.Produce(Vars.content.item("etigeox-silver"))],
+        planet: "etigeox-Neoulandia"
+    });
+
+    chainNode(neoulandia_research1, {
+        unlock: Vars.content.item("etigeox-research-materia2"),
+        objectives: [new Objectives.Produce(Vars.content.item("etigeox-platinum"))],
+        planet: "etigeox-Neoulandia"
+    });
+
+
+    const neoulandia_water = addTechNode({
+        parent: Vars.content.item("edt-timber"),
+        objectives: [new Objectives.Produce(Liquids.water)],
+        unlock: Liquids.water,
+        planet: "etigeox-Neoulandia"
+    });
+    chainNode(neoulandia_water, {
+        unlock: Vars.content.item("etigeox-canned-water"),
+        planet: "etigeox-Neoulandia"
+    });
+    const neoulandia_oil = chainNode(neoulandia_water, {
+        unlock: Liquids.oil,
+        objectives: [new Objectives.Produce(Liquids.oil)],
+        planet: "etigeox-Neoulandia"
+    });
+    chainNode(neoulandia_oil, {
+        unlock: Liquids.slag,
+        objectives: [new Objectives.Produce(Liquids.slag)],
+        planet: "etigeox-Neoulandia"
+    });
     // chainNode(neoulandia_oil, {
     //     unlock: Vars.content.liquid("etigeox-natural-gas"),
     //     objectives: [new Objectives.Produce(Vars.content.liquid("etigeox-natural-gas"))],
