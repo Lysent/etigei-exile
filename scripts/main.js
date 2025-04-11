27eaa73ce588d754ce6a2a654a94e64e653583dc
Mitemi <miter360@gmail.com>
2025-04-11 10:31:12 -0400

    Fix

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-require("content/items/items");
+//require("content/items/items");
 //require("content/blocks/blocks");
 require("content/blocks/productionBlocks");
 //require("content/blocks/powerBlocks");
@@ -7,7 +7,7 @@ require("content/misc/attributes");
 require("content/misc/vanilla");
 require("content/misc/bullets");
 
-require("content/tech/contentLoader");
+//require("content/tech/contentLoader");
 require("content/tech/tech");
 
 require("ui/ui");
